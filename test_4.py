a9e60c45aba4e5a389540f9ac1d3e000c7621059
jSm449g4d <47020182+jSm449g4d@users.noreply.github.com>
2020-09-03 22:03:35 +0900

    本格的にgithubを使用する&ファイル構成を整える #1

--- test_4.py
+++ test_4.py
@@ -25,9 +25,9 @@ def UNET_EZ(input_shape=(None,None,3,)):
     mod=mod_inp = Input(shape=input_shape)
     mod=Conv2D(64,3,padding="same",activation="relu")(mod)
     mod_1=mod
-    mod_1=Conv2D(64,2,2,padding="same",activation="relu")(mod)
+    mod_1=Conv2D(64,2,2,padding="same",activation="relu")(mod_1)
     mod_2=mod_1
-    mod_2=Conv2D(64,4,4,padding="same",activation="relu")(mod)
+    mod_2=Conv2D(64,2,2,padding="same",activation="relu")(mod_2)
     mod_2=Conv2D(64,3,padding="same",activation="relu")(mod_2)
     mod_2=Conv2DTranspose(64,3,2,padding="same",activation="relu")(mod_2)
     mod_1=mod_1+mod_2
@@ -39,6 +39,44 @@ def UNET_EZ(input_shape=(None,None,3,)):
     mod=Conv2D(3,3,padding="same")(mod)
     return keras.models.Model(inputs=mod_inp, outputs=mod)
 
+def Discriminator(input_shape=(None,None,3,)):
+    model = tf.keras.Sequential()
+    model.add(Conv2D(64, (5, 5), strides=2, padding='same'))
+    model.add(LeakyReLU())
+    model.add(Dropout(0.3))
+    model.add(Conv2D(128, (5, 5), strides=2, padding='same'))
+    model.add(LeakyReLU())
+    model.add(Dropout(0.3))
+    model.add(Flatten())
+    model.add(Dense(1))
+    return model
+
+def discriminator_loss(real_output, fake_output):
+    cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+    fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+    total_loss = real_loss + fake_loss
+    return total_loss
+
+def train_step(gen,disc,input_x,true_y):
+    def generator_loss(fake_output):
+        cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+        return cross_entropy(tf.ones_like(fake_output), fake_output)
+    with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+      output_y = gen(input_x, training=True)
+
+      real_output = disc(input_x, training=True)
+      fake_output = disc(output_y, training=True)
+
+      gen_loss = generator_loss(fake_output)
+      disc_loss = discriminator_loss(real_output, fake_output)
+
+    gradients_of_generator = gen_tape.gradient(gen_loss, gen.trainable_variables)
+    gradients_of_discriminator = disc_tape.gradient(disc_loss, disc.trainable_variables)
+
+    optimizers.Adam(lr=0.0005, beta_1=0.9, beta_2=0.999).apply_gradients(zip(gradients_of_generator, gen.trainable_variables))
+    optimizers.Adam(lr=0.0005, beta_1=0.9, beta_2=0.999).apply_gradients(zip(gradients_of_discriminator, disc.trainable_variables))
+
 def train():
     limitDataSize=min([args.limit_data_size,len(ffzk(args.train_input))])
     x_train=img2np(ffzk(args.train_input)[:limitDataSize],img_len=128)
@@ -47,16 +85,14 @@ def train():
     y_test=img2np(ffzk(args.pred_output),img_len=128)
     #[:10]*1000
     
-    model=UNET_EZ()
-    model.compile(optimizer=optimizers.Adam(lr=0.0005, beta_1=0.9, beta_2=0.999),
-                  loss=keras.losses.mean_squared_error)#keras.losses.mean_squared_error
-    model.summary()
-    cbks=[]
-    if(args.TB_logdir!=""):
-        cbks=[keras.callbacks.TensorBoard(log_dir=args.TB_logdir, histogram_freq=1)]
+    gen=UNET_EZ()
+    disc=Discriminator()
     
-    model.fit(x_train, y_train,epochs=args.epoch,batch_size=args.batch,validation_data=(x_test, y_test),callbacks=cbks)
-    model.save(args.save)
+    for epoch in range(args.epoch):
+        for x,y in zip(x_train,y_train):
+            train_step(gen,disc,x,y)
+    
+    gen.save(args.save)
     
 def test():
     model = keras.models.load_model(args.save)
